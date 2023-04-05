04eadd5e30e0207cc9536be8b19af8581ae53280
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2023-04-05 10:02:22 -0400

    Add CI YAML file and some CSS to `/new` endpoint

--- test/itch_clone_web/controllers/page_controller_test.exs
+++ test/itch_clone_web/controllers/page_controller_test.exs
@@ -3,6 +3,20 @@ defmodule ItchCloneWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Hello world!"
   end
+
+  test "GET /new", %{conn: conn} do
+    conn = get(conn, ~p"/new")
+    assert html_response(conn, 200) =~ "Create a new project"
+    assert html_response(conn, 200) =~ "Title"
+    assert html_response(conn, 200) =~ "Project URL"
+    assert html_response(conn, 200) =~ "Short description or tagline"
+    assert html_response(conn, 200) =~ "Uploads"
+    assert html_response(conn, 200) =~ "Upload files"
+    assert html_response(conn, 200) =~ "Details"
+    assert html_response(conn, 200) =~ "Game Description"
+    assert html_response(conn, 200) =~ "Save & view page"
+  end
+
 end
