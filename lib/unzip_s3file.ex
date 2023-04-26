a4193975d656996552716e6a40e4958b26d34f24
skp96 <patel.sunny293@gmail.com>
2023-04-26 13:47:16 -0400

    add unzip submit error handling

--- lib/unzip_s3file.ex
+++ lib/unzip_s3file.ex
@@ -5,13 +5,21 @@ defmodule Unzip.S3File do
 
   def run(bucket, zip_path) do
     zip_file_binary = File.read!(zip_path)
-    {:ok, dir_path} = Temp.mkdir "temp-dir"
+    {:ok, dir_path} = Temp.mkdir("temp-dir")
     {:ok, filelist} = unzip(zip_file_binary, dir_path)
     num = length(filelist) - 1
 
     Aws.add_multiple(bucket, filelist, num)
 
-    File.rm_rf dir_path
+    File.rm_rf(dir_path)
+  end
+
+  def is_zipped(file_name) do
+    if Path.extname(file_name) === ".zip" do
+      {:ok, true}
+    else
+      {:error, "File must be a zipped file (.zip extension)"}
+    end
   end
 
   def unzip(file_binary, directory) do
--- test/itch_clone_web/controllers/upload_controller_test.exs
+++ test/itch_clone_web/controllers/upload_controller_test.exs
@@ -5,7 +5,16 @@ defmodule ItchCloneWeb.UploadControllerTest do
   import Mockery
   import Mockery.Assertions
 
-  @valid_file %Plug.Upload{filename: "Game.zip", content_type: "application/zip", path: "/absolute_path_to/Game.zip" }
+  @valid_file %Plug.Upload{
+    filename: "Game.zip",
+    content_type: "application/zip",
+    path: "/absolute_path_to/Game.zip"
+  }
+  @unzipped_file %Plug.Upload{
+    filename: "Game_Not_Zipped",
+    content_type: "application/zip",
+    path: "/absolute_path_to/Game_Not_Zipped"
+  }
 
   test "GET /games/new", %{conn: conn} do
     conn = get(conn, ~p"/games/new")
@@ -14,13 +23,18 @@ defmodule ItchCloneWeb.UploadControllerTest do
   end
 
   describe "create upload" do
-
     test "game is successfully uploaded", %{conn: conn} do
-      mock Unzip.S3File, :run, :ok
+      mock(Unzip.S3File, :run, :ok)
 
       conn = post(conn, ~p"/games", upload: @valid_file)
       assert html_response(conn, 200) =~ "Play the Game!"
-      assert_called Unzip.S3File, :run
+      assert_called(Unzip.S3File, :run)
+    end
+
+    test "respond with 422 on unzipped files being uploaded", %{conn: conn} do
+      conn = post(conn, ~p"/games", upload: @unzipped_file)
+
+      assert(html_response(conn, 422))
     end
   end
 end
