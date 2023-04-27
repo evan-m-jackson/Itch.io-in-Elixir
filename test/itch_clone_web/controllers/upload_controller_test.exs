1477d55c16e64ad08fe070bd3319d3e03d22f592
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2023-04-27 16:19:04 -0400

    Refactor  tests

--- test/itch_clone_web/controllers/upload_controller_test.exs
+++ test/itch_clone_web/controllers/upload_controller_test.exs
@@ -3,7 +3,7 @@ defmodule ItchCloneWeb.UploadControllerTest do
   alias Aws
   alias GameFile
   import Mockery
-  import Plug.Test
+  import Phoenix.ConnTest
   import Mockery.Assertions
 
   @valid_file %Plug.Upload{
@@ -18,29 +18,47 @@ defmodule ItchCloneWeb.UploadControllerTest do
   }
   @user %ItchClone.User{id: 1, email: "macewindu@example.com", token: "123456abcdef"}
 
-  test "GET /games/new", %{conn: conn} do
-    conn = Phoenix.ConnTest.build_conn()
-      |> assign(:user, @user)
 
-    conn = get(conn, ~p"/games/new")
-    assert html_response(conn, 200) =~ "Create a new project"
-    assert html_response(conn, 200) =~ "Upload files"
+  describe "add a new game page" do
+    test "user is logged in and can access page" do
+      ItchClone.Repo.insert(@user)
+      conn =
+        Phoenix.ConnTest.build_conn()
+        |> Phoenix.ConnTest.init_test_session(user_id: @user.id)
+
+      conn = get(conn, ~p"/games/new")
+      assert html_response(conn, 200) =~ "Create a new project"
+      assert html_response(conn, 200) =~ "Upload files"
+    end
+
+    test "user is not logged in and gets redirected to home page" do
+      conn = Phoenix.ConnTest.build_conn()
+      conn = get(conn, ~p"/games/new")
+      assert html_response(conn, 302)
+    end
   end
 
+
   describe "create upload" do
-    test "game is successfully uploaded", %{conn: conn} do
+    test "game is successfully uploaded" do
       mock Unzip.S3File, :run, :ok
-      conn = Phoenix.ConnTest.build_conn()
-      |> assign(:user, @user)
+      ItchClone.Repo.insert(@user)
+      conn =
+        Phoenix.ConnTest.build_conn()
+        |> Phoenix.ConnTest.init_test_session(user_id: @user.id)
 
       conn = post(conn, ~p"/games", upload: @valid_file)
       assert html_response(conn, 200) =~ "Play the Game!"
       assert_called(Unzip.S3File, :run)
     end
 
-    test "respond with 422 on unzipped files being uploaded", %{conn: conn} do
-      conn = post(conn, ~p"/games", upload: @unzipped_file)
+    test "respond with 422 on unzipped files being uploaded" do
+      ItchClone.Repo.insert(@user)
+      conn =
+        Phoenix.ConnTest.build_conn()
+        |> Phoenix.ConnTest.init_test_session(user_id: @user.id)
 
+      conn = post(conn, ~p"/games", upload: @unzipped_file)
       assert(html_response(conn, 422))
     end
   end
